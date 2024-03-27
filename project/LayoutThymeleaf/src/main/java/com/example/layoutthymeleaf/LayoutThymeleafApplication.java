052fcbf8fa20f7a8c2c44db34f388ddce0cbdc94
Fidan Sadikhova <99359863+fid03@users.noreply.github.com>
2024-03-28 01:26:50 +0400

    Add files via upload

---
+++ project/LayoutThymeleaf/src/main/java/com/example/layoutthymeleaf/LayoutThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.layoutthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayoutThymeleafApplication.class, args);
+    }
+
+}
